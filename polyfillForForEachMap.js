c25b05f8578930de6845d37dc37b9f56faee1a62
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-06-06 10:37:53 +0530

    polyfill for forEach and Map

---
+++ polyfillForForEachMap.js
@@ -0,0 +1,21 @@
+var myArray = ["Hulk", "Ruben", "John", "Nano"];
+
+//for forEach
+Array.prototype.eachFor = function (callback) {
+  for (var i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+myArray.eachFor(function (word) {
+  console.log(word);
+});
+
+//for map
+Array.prototype.myMap = function (callback) {
+  var arr = [];
+  for (var i = 0; i < this.length; i++) {
+    arr.push(callback(this[i], i, this));
+  }
+  return arr;
+};
